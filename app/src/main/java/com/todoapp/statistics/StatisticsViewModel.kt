a1c441112c5e3bc49fa9d29bbda1a690bd76fa4e
Awais Mansha <awaismansha1998@gmail.com>
2021-09-28 09:05:12 +0500

    working on lesson 10 in kotlin

---
+++ app/src/main/java/com/todoapp/statistics/StatisticsViewModel.kt
@@ -0,0 +1,45 @@
+package com.todoapp.statistics
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.todoapp.data.Result
+import com.todoapp.data.Task
+import com.todoapp.data.source.DefaultTasksRepository
+import kotlinx.coroutines.launch
+import com.todoapp.data.Result.Success
+
+/**
+ * ViewModel for the statistics screen.
+ */
+class StatisticsViewModel(application: Application) : AndroidViewModel(application) {
+
+    // Note, for testing and architecture purposes, it's bad practice to construct the repository
+    // here. We'll show you how to fix this during the codelab
+    private val tasksRepository = DefaultTasksRepository.getRepository(application)
+
+    private val tasks: LiveData<Result<List<Task>>> = tasksRepository.observeTasks()
+    private val _dataLoading = MutableLiveData<Boolean>(false)
+    private val stats: LiveData<StatsResult?> = tasks.map {
+        if (it is Success) {
+            getActiveAndCompletedStats(it.data as List<Task>?)
+        } else {
+            null
+        }
+    }
+
+    val activeTasksPercent = stats.map {
+        it?.activeTasksPercent ?: 0f }
+    val completedTasksPercent: LiveData<Float> = stats.map { it?.completedTasksPercent ?: 0f }
+    val dataLoading: LiveData<Boolean> = _dataLoading
+    val error: LiveData<Boolean> = tasks.map { it is Error }
+    val empty: LiveData<Boolean> = tasks.map { (it as? Success)?.data == null }
+//    val empty: LiveData<Boolean> = tasks.map { (it as? Success)?.data.isNullOrEmpty() }
+
+    fun refresh() {
+        _dataLoading.value = true
+            viewModelScope.launch {
+                tasksRepository.refreshTasks()
+                _dataLoading.value = false
+            }
+    }
+}
